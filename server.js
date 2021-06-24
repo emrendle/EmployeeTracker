a1686b25d167f3628401aec05dc7556267ccf5f2
Emily Rendleman <emilyrendleman@gmail.com>
2021-06-23 20:46:38 -0500

    did some extreme code refactoring - much cleaner now

--- server.js
+++ server.js
@@ -96,138 +96,144 @@ const getEmployeesByDept = () => {
     });
 }
 
+// needs to be changed to dynamically load options
 const addEmployee = () => {
-  inquirer
-    .prompt([
-      {
-        name: 'firstName',
-        type: 'input',
-        message: "What is the employee's first name?",
-      },
-      {
-        name: 'lastName',
-        type: 'input',
-        message: "What is the employee's last name?",
-      },
-      {
-        name: 'manager',
-        type: 'rawlist',
-        message: "Who is this employee's manager?",
-        choices: [
-        "Michael Scott",
-        "Robert California",
-        ]
-      },
-      {
-        name: 'role',
-        type: 'rawlist',
-        message: "What is this employee's role?",
-        choices: [
-        "Salesperson",
-        "Receptionist",
-        "Accountant"
-        ]
-      },
-    ])
-    .then((answer) => {
-      if (answer.role === "Salesperson") {
-      answer.role = 4;
-      } else if (answer.role === "Receptionist") {
-      answer.role = 2;
-      } else if (answer.role === "Accountant") {
-      answer.role = 3;
+  let managerInfo = [];
+  let managerChoice = [];
+  let roleChoice = [];
+
+  connection.query('SELECT employee.first_name, employee.last_name, employee.id FROM employee WHERE role_id = ?', [1], (err, res) => {
+    if (err) throw err;
+    res.forEach(value => {
+      managerName = `${value.first_name} ${value.last_name}`
+      managerChoice.push(managerName);
+      let managerObj = {
+        first_name: value.first_name,
+        last_name: value.last_name,
+        id: value.id,
       }
+      managerInfo.push(managerObj);
+    });
 
-      if (answer.manager === "Michael Scott") {
-      answer.manager = 1;
-      } else if (answer.manager === "Robert California") {
-      answer.manager = 5;
-      }
-
-      connection.query(
-        'INSERT INTO employee SET ?',
-          {
-          first_name: answer.firstName,
-          last_name: answer.lastName,
-          role_id: answer.role,
-          manager_id: answer.manager,
-          },
-
-        (err, res) => {
-          if (err) throw err;
-          console.log(`${answer.firstName} ${answer.lastName} has been added.`);
-          loadPrompts();
+    connection.query('SELECT role.title, role.id FROM role', (err, res) => {
+      if (err) throw err;
+      res.forEach(value => {
+        roleChoice.push(value.title);
+      });
+      inquirer
+      .prompt([
+        {
+          name: 'firstName',
+          type: 'input',
+          message: "What is the employee's first name?",
+        },
+        {
+          name: 'lastName',
+          type: 'input',
+          message: "What is the employee's last name?",
+        },
+        {
+          name: 'manager',
+          type: 'rawlist',
+          message: "Who is this employee's manager?",
+          choices: managerChoice,
+        },
+        {
+          name: 'role',
+          type: 'rawlist',
+          message: "What is this employee's role?",
+          choices: roleChoice,
+        },
+      ])
+      .then((answer) => {  
+        const splitManager = answer.manager.split(' ');
+        for (i = 0; i < managerInfo.length; i++) {
+          if (splitManager[0] === managerInfo[i].first_name && splitManager[1] === managerInfo[i].last_name) {
+            foundManagerId = managerInfo[i].id;
+          }
         }
-      );
+        res.forEach(value => {
+          if (answer.role === value.title) {
+            foundRoleId = value.id;
+          }
+        });
+
+        connection.query(
+          'INSERT INTO employee SET ?',
+            {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            role_id: foundRoleId,
+            manager_id: foundManagerId,
+            },
+  
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${answer.firstName} ${answer.lastName} has been added.`);
+            loadPrompts();
+            }
+          );
+        });
+      });
   });
 }
 
 const remEmployee = () => {
   let remChoices = [];
-  let joinedRemChoices = [];
 
   // finding all employee first and last names from mysql
   connection.query("SELECT employee.first_name, employee.last_name FROM employee", (err, res) => {
-    if(err) {
-    throw err;
-    } else {
-    setRemoveValue(res);
-    }
+    if (err) throw err;
+      res.forEach(choice => {
+        fullName = `${choice.first_name} ${choice.last_name}`;
+        remChoices.push(fullName);
+      });
+  
+      inquirer.prompt([
+        {
+          name: "employeeRemove",
+          type: "rawlist",
+          message: "Which employee would you like to remove?",
+          choices: remChoices,
+        }
+      ])
+        .then((answer) => {
+          // splits the previously joined string into individual words that serve as first and last name
+          const splitAnswer = answer.employeeRemove.split(' ');
+          // removes employee from db where there is a name match - it would probably be better (less error prone) to delete by id in case there are employees with identical names
+          connection.query(`DELETE FROM employee WHERE first_name = ? AND last_name = ?`, [splitAnswer[0], splitAnswer[1]] , (err, res) => {
+            if (err) throw err;
+            console.log(`${remFirstName} ${remLastName} has been removed.`);
+            loadPrompts();
+          });
+      });
   });
-
-  // loops through all employees, joins them as a single string, pushes to an array, then presents that array as choices for inquirer prompt
-  const setRemoveValue = (value) => {
-    remChoices = value;
-
-    remChoices.forEach(choice => {
-      fullName = `${choice.first_name} ${choice.last_name}`;
-      joinedRemChoices.push(fullName);
-    });
-
-    inquirer.prompt([
-      {
-        name: "employeeRemove",
-        type: "rawlist",
-        message: "Which employee would you like to remove?",
-        choices: joinedRemChoices,
-      }
-    ])
-      .then((answer) => {
-        // splits the previously joined string into individual words that serve as first and last name
-        const splitAnswer = answer.employeeRemove.split(' ');
-        const remFirstName = splitAnswer[0];
-        const remLastName = splitAnswer[1];
-        // removes employee from db where there is a name match - it would probably be better (less error prone) to delete by id in case there are employees with identical names
-        connection.query(`DELETE FROM employee WHERE first_name = ? AND last_name = ?`, [remFirstName, remLastName] , (err, res) => {
-          if(err) {
-          throw err;
-          } else {
-          console.log(`${remFirstName} ${remLastName} has been removed.`);
-          loadPrompts();
-          }
-        });
-    });
-  }
 }
 
 const updEmployeeRole = () => {
   let updEmplChoices = [];
   let roleChoices = [];
+  let roleInfo = [];
 
   // queries first and last name from employee table
   connection.query("SELECT employee.first_name, employee.last_name FROM employee", (err, res) => {
-    if(err) {
-    throw err;
-    } else {
-    setUpdValue(res);
-    }
-  });
+    if (err) throw err; 
 
-  // loops through data from mysql query, pushes it to array, displays that array as choices for which employee to udpate
-  const setUpdValue = (value) => {
-    value.forEach(choice => {
+    res.forEach(choice => {
       fullName = `${choice.first_name} ${choice.last_name}`;
       updEmplChoices.push(fullName);
+    });
+  });
+
+  connection.query("SELECT role.title, role.id FROM role", (err, res) => {
+    if (err) throw err;
+    res.forEach(role => {
+      let roleObj = {
+        title: role.title,
+        id: role.id,
+      }
+      roleChoices.push(role.title);
+      roleInfo.push(roleObj);
     });
 
     inquirer.prompt([
@@ -237,53 +243,93 @@ const updEmployeeRole = () => {
         message: "Which employee would you like to update?",
         choices: updEmplChoices,
       },
+      {
+        name: "roleUpd",
+        type: "rawlist",
+        message: "What is this employee's new role?",
+        choices: roleChoices,
+      },
     ])
       .then((answer) => {
-        // queries employee's role id's and titles associated with those role id's to display as choices for inquirer'
         let chosenName = answer.employeeUpd.split(' ');
-        connection.query("SELECT role.title FROM employee INNER JOIN role ON role.id = employee.role_id GROUP BY role.id", (err, res) => {
-          if(err) {
-          throw err;
-          } else {
-            setRoleValue(res);
+
+        res.forEach(value => {
+          if (answer.roleUpd === value.title) {
+            newRoleId = value.id;
           }
         });
 
-        // loops through data from mysql query, pushes to an array, uses that array as choices in inquirer prompt
-        const setRoleValue = (value) => {
-          value.forEach(choice => {
-            roleChoices.push(choice.title);
-          });
-      
-          inquirer.prompt([
-            {
-              name: "roleUpd",
-              type: "rawlist",
-              message: "What is this employee's new role?",
-              choices: roleChoices,
-            },
-          ])
-            .then((answer) => {
-              // finds the matching role id for the chosen role
-              connection.query(`SELECT role.id FROM role WHERE ?`, { title: answer.roleUpd } , (err, res) => {
-                if (err) {
-                throw err;
-                } else {
-                  // updates that employee's role id with the id sources from above where there is a name match
-                  connection.query("UPDATE employee SET ? WHERE first_name = ? AND last_name = ?", [{ role_id: res[0].id }, chosenName[0], chosenName[1]], (err, res) => {
-                    if(err) {
-                    throw err;
-                    } else {
-                      console.log(`${chosenName.join(' ')}'s role has been updated.`)
-                      loadPrompts();
-                    }
-                  });
-                }
-              });
-          });
-        }
+        connection.query("UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?", [newRoleId, chosenName[0], chosenName[1]], (err, res) => {
+          if (err) throw err; 
+            console.log(`${chosenName.join(' ')}'s role has been updated.`)
+            loadPrompts();
+        });
     });
-  }
+  });
+}
+
+const getAllRoles = () => {
+  connection.query('SELECT role.title, role.salary, role.department_id FROM role', (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    loadPrompts();
+  });
+}
+
+const addRole = () => {
+  let roleChoices = [];
+
+  connection.query("SELECT department.name, department.id FROM department", (err, res) => {
+    if(err) {
+      throw err;
+    } else {
+      res.forEach(value => {
+        roleChoices.push(value.name);
+      });
+
+      inquirer
+      .prompt([
+        {
+          name: 'roleTitle',
+          type: 'input',
+          message: "What is the title of this role?",
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: "What is the salary for this position?",
+        },
+        {
+          name: "department",
+          type: "rawlist",
+          message: "Which department should this role be added to?",
+          choices: roleChoices,
+        },
+      ])
+      .then((answer) => {
+        res.forEach(value => {
+          if (answer.department === value.name) {
+            departmentId = value.id;
+          }
+        });
+
+        connection.query(
+          'INSERT INTO role SET ?',
+            {
+            title: answer.roleTitle,
+            salary: answer.salary,
+            department_id: departmentId,
+            },
+        
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${answer.roleTitle} has been added as a role.`);
+            loadPrompts();
+          }
+        );
+      });
+    }
+  });
 }
 
 connection.connect((err) => {
